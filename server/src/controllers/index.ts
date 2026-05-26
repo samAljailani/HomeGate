0e3cd58de0fd12addbc9000d7b269a883c014ce3
Sami Aljailani <samialjailani2@gmail.com>
2026-05-25 22:42:04 -0400

    Removed test controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,4 +1,3 @@
 import { AuthController } from './auth.controller'
-import { TestController } from './test.controller'
 
-export const controllers = [AuthController, TestController]
+export const controllers = [AuthController]
--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,5 +1,4 @@
 import { AuthService } from './auth.service'
-import { TestService } from './test.service'
 import { UserService } from './user.service'
 
-export const services = [UserService, AuthService, TestService]
+export const services = [UserService, AuthService]
