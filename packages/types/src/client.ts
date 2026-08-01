9cc4391daffd49aaad5ea4a983558749329b3ed2
Sami Aljailani <samialjailani2@gmail.com>
2026-07-31 23:49:33 -0400

    fix: strict typing for createApiClient return type

--- packages/types/src/client.ts
+++ packages/types/src/client.ts
@@ -1,7 +1,7 @@
-import createClient, { type ClientOptions } from 'openapi-fetch'
+import createClient, { type Client, type ClientOptions } from 'openapi-fetch'
 
 import type { paths } from './schema'
 
-export const createApiClient = (options: ClientOptions) => createClient<paths>(options)
+export const createApiClient = (options: ClientOptions): Client<paths> => createClient<paths>(options)
 
 export type ApiClient = ReturnType<typeof createApiClient>
